a87bd49a2fe51feddda078cdba50d1609a79edc5
AlexaDudek <dudeka@bu.edu>
2021-12-14 11:13:08 -0500

    Work as of 12/14 morning.

--- TradeOffAnalysis.R
+++ TradeOffAnalysis.R
@@ -12,8 +12,8 @@ generateTradeOffData <- function(FmListX, FmListY, x0, y0, rx, ry, Kx, Ky, c, nu
     
     timeSeries = simulatePopulations_noise(x0, y0, rx, ry, Kx, Ky, c, FmX, FmY)
     
-    meanX = mean(timeSeries$x[(numTimeSteps - numMeanYears):numTimeSteps])
-    meanY = mean(timeSeries$y[(numTimeSteps - numMeanYears):numTimeSteps])
+    meanX = mean(timeSeries$x[(numTimeSteps - (numMeanYears-1)):numTimeSteps])
+    meanY = mean(timeSeries$y[(numTimeSteps - (numMeanYears-1)):numTimeSteps])
     
     tradeOffData$xBiomass[row] = meanX
     tradeOffData$yBiomass[row] = meanY
@@ -22,8 +22,18 @@ generateTradeOffData <- function(FmListX, FmListY, x0, y0, rx, ry, Kx, Ky, c, nu
 }
 
 
-# test based on Perretti et al 2013 parameter values
+
 FmListX = seq(0, 0.2, by = 0.01) 
 FmListY = seq(0, 0.2, by = 0.01)
+
+# test based on Perretti et al 2013 parameter values
 testTradeOff1 <- generateTradeOffData(FmListX, FmListY, x0 = 0.4, y0 = 0.4, rx = 3.8, ry = 3.7, 
                                       Kx = 1, Ky = 1, c = 0.1)
+
+# 200 time steps, averaged across all
+testTradeOff2 <- generateTradeOffData(FmListX, FmListY, x0 = 0.4, y0 = 0.4, rx = 3.8, ry = 3.7, 
+                                      Kx = 1, Ky = 1, c = 0.1, numTimeSteps = 200, numMeanYears = 200)
+
+
+
+
