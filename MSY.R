6a73178df57d927c1cc2ac3aff6fa26e1184fdd9
AlexaDudek <dudeka@bu.edu>
2021-12-16 14:44:05 -0500

    CCM update

--- MSY.R
+++ MSY.R
@@ -1,32 +1,16 @@
 
-singleSpeciesMSY <- function(S, r, K){
-  dS = S*exp(r*(1-(S/K))) - S
-  return(dS)
-}
-
 singleSpeciesRicker <- function(S, r, K){
   nextS = S*exp(r*(1-(S/K)))
   return(nextS)
 }
 
-dataframeMSY <- function(S, r, K, numYear){
-  populations =  data.frame(Nt = numeric(numYear), NtNext = numeric(numYear))
-  for(row in 1:numYear){
-    populations$Nt[row] = S
-    nextS = S*exp(r*(1-(S/K))) - change
-    populations$NtNext[row] = nextS
-    S = nextS
-  }
-  return(populations)
+singleSpeciesMSY <- function(S, r, K){
+  dS = S*exp(r*(1-(S/K))) - S
+  return(dS)
 }
 
 
-BmsyNextS <- function(r, K){
-  msy = optim(K/2, fn = function(x) -singleSpeciesRicker(x, r, K), method="SANN")$par
-  return(msy)
-}
-
-BmsyDS <- function(r, K){
+Bmsy <- function(r, K){
   msy = optim(K/2, fn = function(x) -singleSpeciesMSY(x, r, K), method="SANN")$par
 }
 
@@ -41,10 +25,31 @@ Ymsy <- function(r, K){
 Fmsy <- function(r, K){
   Bmsy = Bmsy(r, K)
   Ymsy = Ymsy(r, K)
-  Fmsy = Ymsy/Bmsy
+  Fmsy = Ymsy/singleSpeciesRicker(Bmsy, r, K)
   return(Fmsy)
 }
 
+
+FFmsy = Fmsy(r = 2, K = 1)
+Pmsy = Fmsy(r = 3.5/4.5, K = 0.1)
+
+
+### Commented out UNUSED CODE
+# BmsyNextS <- function(r, K){
+#   msy = optim(K/2, fn = function(x) -singleSpeciesRicker(x, r, K), method="SANN")$par
+#   return(msy) 
+# }
+
+# dataframeMSY <- function(S, r, K, numYear){
+#   populations =  data.frame(Nt = numeric(numYear), NtNext = numeric(numYear))
+#   for(row in 1:numYear){
+#     populations$Nt[row] = S
+#     nextS = S*exp(r*(1-(S/K))) - change
+#     populations$NtNext[row] = nextS
+#     S = nextS
+#   }
+#   return(populations)
+# }
 
 # This was me trying to figure out what was happening
 # Everything is in random orders and not well defined
@@ -79,17 +84,23 @@ ggplot() +
   geom_function(fun = function(x) 0, color = "grey40") +
   geom_function(fun = function(x) singleSpeciesRicker(x, r, K)) + 
   xlim(0,2) +
-  theme_bw()
+  theme_bw() +
+  labs(x = "Nt", y = "Nt+1")
 
 ggplot() +
-  geom_function(fun = function(x) 0, color = "grey40") +
+  geom_function(fun = function(x) 0, color = "grey") +
   geom_function(fun = function(x) singleSpeciesMSY(x, r, K)) + 
   xlim(0,2) +
-  theme_bw()
+  theme_bw() +
+  labs(x = "Nt", y = "delta N")
+
 
 # These values actually outputted pretty cool plots 
 Fms = seq(0, 1, by = 0.1)
 Fms2 = seq(0, 0.5, by = 0.05)
+FmSBmsy = seq(0.25, 0.35, 0.01)
+FmMmsy = seq(0.45, 0.65, 0.01)
+FmMmsyFine = seq(0.54, 0.56, 0.001)
 
 toMSYTest = generateTradeOff_Ricker(Fms, Fms, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.2, cy = 0.2, 50, 50)
 
@@ -98,3 +109,11 @@ toMSYTest2 = generateTradeOff_Ricker(Fms, Fms, x0 = 0.5, y0 = 0.05, rx = 2.0, ry
 toMSYTest3 = generateTradeOff_Ricker(Fms, Fms, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.1, cy = 0.1, 50, 50)
 
 toMSYTest4 = generateTradeOff_Ricker(Fms2, Fms2, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.1, cy = 0.1, 50, 50)
+
+toMSYTest5 = generateTradeOff_Ricker(Fms, Fms, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.2, cy = 0.2, 50, 50)
+
+toMSYTest6 =  generateTradeOff_Ricker(FmSBmsy, FmSBmsy, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.2, cy = 0.2, 50, 50)
+
+toMSYTest7 = generateTradeOff_Ricker(FmMmsy, FmMmsy, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.2, cy = 0.2, 50, 50)
+
+toFineMSYM = generateTradeOff_Ricker(FmMmsyFine, FmMmsyFine, x0 = 0.5, y0 = 0.05, rx = 2.0, ry = (3.5/4.5), Kx = 1, Ky = 0.1, cx = 0.2, cy = 0.2, 50, 50)
--- testingCMC_deyle.R
+++ testingCMC_deyle.R
@@ -1,4 +1,4 @@
-forageFishRicker_noise <- function(x0, y0, r, K, c, f){
+forageFishRicker_noise_nl <- function(x0, y0, r, K, c, f){
   
   #population without fishing
   x1 = ((x0*exp(r*(1-(x0/K)))) - (c*x0*y0))*rlnorm(1, meanlog=0, sdlog=0.1)
@@ -14,7 +14,7 @@ forageFishRicker_noise <- function(x0, y0, r, K, c, f){
 }
 
 
-predatorRicker_noise <- function(y0, x0, r, K, c, f){
+predatorRicker_noise_nl <- function(y0, x0, r, K, c, f){
   
   #population without fishing
   y1 = ((y0*exp(r*(1-(y0/K)))) + ((c*x0*y0)))*rlnorm(1, meanlog=0, sdlog=0.1)
@@ -25,34 +25,77 @@ predatorRicker_noise <- function(y0, x0, r, K, c, f){
 }
 
 
-simulateRicker_noise <- function(x0, y0, rx, ry, Kx, Ky, cx, cy, fx, fy, numTimeSteps = 50){
-  populations =  data.frame(time = 1:numTimeSteps, x = numeric(numTimeSteps), y = numeric(numTimeSteps))
+simulateRicker_noise_nl <- function(x0, y0, rx, ry, Kx, Ky, cx, cy, fx, fy, numTimeSteps = 50){
+  populations =  data.frame(time = 1:numTimeSteps, x = numeric(numTimeSteps), y = numeric(numTimeStep), 
+                            xYield = numeric(numTimeSteps), yYield = numeric(numTimeSteps))
+  
   for(i in 1:numTimeSteps){
     
     fx_i <- mean(rbinom(1000,1,fx))
     fy_i <- mean(rbinom(1000,1,fy))
     
-    populations$x[i] = forageFishRicker_noise(x0, y0, rx, Kx, cx, fx_i)
-    populations$y[i] = predatorRicker_noise(y0, x0, ry, Ky, cy, fy_i)
+    populations$x[i] = forageFishRicker_noise_nl(x0, y0, rx, Kx, cx, fx_i)
+    populations$y[i] = predatorRicker_noise_nl(y0, x0, ry, Ky, cy, fy_i)
     
     x0 = populations$x[i]
     y0 = populations$y[i]
+    
+    populations$xYield[i] = fx_i * x0
+    populations$yYield[i] = fy_i * y0
   }
   return(populations) #returns time series with data for both populations in a data frame
-} 
+}
 
 
-
-  CCMdata = simulateRicker_noise(x0 = 1.0, y0 = 0.05, rx = 3.0, ry = 3, Kx = 1, Ky = 0.1, cx = 0.01, cy = 0.2, fx=0.1, fy=0.1, 500)
+  # greater r
+  # simulated over 500 time steps 
+  # just take the last 100 
+  # cx is way lower than cy 
+  # fishing at 0.1
+  CCMdata = simulateRicker_noise_nl(x0 = 1.0, y0 = 0.05, rx = 3.0, ry = 3, Kx = 1, Ky = 0.1, cx = 0.01, cy = 0.2, fx=0.1, fy=0.1, 500)
   CCMdata <- CCMdata[-c(1:100),]
-  CCM_rho_out_highcy <- EmbedDimension(dataFrame = CCMdata, Tp = 0, columns = "y", target = "x", lib="1 250",pred="1 250", showPlot = FALSE)
+  CCM_rho_out_highcy <- EmbedDimension(dataFrame = CCMdata, Tp = 0, columns = "y", target = "x", lib="1 100",pred="1 100", showPlot = FALSE)
   
   
   
-  CCMdata = simulateRicker_noise(x0 = 1.0, y0 = 0.05, rx = 3.0, ry = 3, Kx = 1, Ky = 0.1, cx = 0.01, cy = 0.02, fx=0.1, fy=0.1, 500)
+  
+  # same thing but 
+  CCMdata = simulateRicker_noise_nl(x0 = 1.0, y0 = 0.05, rx = 3.0, ry = 3, Kx = 1, Ky = 0.1, cx = 0.01, cy = 0.02, fx=0.1, fy=0.1, 500)
   CCMdata <- CCMdata[-c(1:100),]
-  CCM_rho_out_lowcy <- EmbedDimension(dataFrame = CCMdata, Tp = 0, columns = "y", target = "x", lib="1 250",pred="1 250", showPlot = FALSE)
-
+  CCM_rho_out_lowcy <- EmbedDimension(dataFrame=df, lib="1 100", pred="1 100", columns="xYield", target="xYield", showPlot = FALSE)
+  
   
   max(CCM_rho_out_highcy$rho)
   max(CCM_rho_out_lowcy$rho)
+  
+  CCMyield_data = CCM(dataFrame = CCMdata, E = 3, Tp = 0, columns = "yYield", target = "xYield", libSizes = "10 48 5", sample = 100, showPlot = TRUE)
+  
+  
+  
+  ### Get to Final Prediction Coefficient from data frame
+  # get embedding dimension of all columns - use lowest one?
+  # 
+  
+  getE <- function(df){
+    E_FFYlist = EmbedDimension(dataFrame=df, lib="1 100", pred="1 100", columns="yYield", target="xYield", showPlot = FALSE)
+    E_FFY = E_FFYlist$E[E_FFYlist$rho == max(E_FFYlist$rho)]
+    
+    E_PYlist = EmbedDimension(dataFrame=CCMdata, lib="1 100", pred="1 100", columns="Yield", target="xYield", showPlot = FALSE)
+    E_PY = E_PYlist$E[E_PYlist$rho == max(E_PYlist$rho)]
+    
+    E_PBlist = EmbedDimension(dataFrame=CCMdata, lib="1 100", pred="1 100", columns="y", target="y", showPlot = FALSE)
+    E_PB = E_PBlist$E[E_PBlist$rho == max(E_PBlist$rho)]
+    
+    Elist = c(E_FFY, E_PY, E_PB)
+    return(min(Elist))
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
